dd34d6fe66ce0d3023b6fc64c8f664c2a09f8335
Haervwe <juan-jose-007@hotmail.com>
2025-02-12 22:51:39 -0300

    Update perplexica_pipe.py

--- perplexica_pipe.py
+++ perplexica_pipe.py
@@ -1,14 +1,14 @@
 """
 title: perplexica pipe
 author: open-webui, gwaanl
-author_url：https://github.com/ViffyGwaanl/perplexica-pipe
+author_url: https://github.com/ViffyGwaanl/perplexica-pipe
 You can find the instructions and submit questions at the website above
 funding_url: https://github.com/ViffyGwaanl
-version: 0.3.5
+version: 0.3.7
 required_open_webui_version: 0.5.3
 
 0.1.1：Create code to implement the "pipeline" function using the Perplexica search API
-0.2.1：Increased contextual memory function, which can record historical conversation data 
+0.2.1：Increased contextual memory function, which can record historical conversation data 
 and send the history to the Perplexica API for more accurate searches each time a search is performed.
 0.2.2：Change the model display name to the following format "Perplexica/gpt-4o/academicSearch"
 0.3.1：Added the option to select a local model, which can be configured to use OpenAI or Ollama in perplexica_provider
@@ -17,11 +17,29 @@ and send the history to the Perplexica API for more accurate searches each time 
 0.3.4: Fixed the issue that caused openwebui to fail to start when the values of customOpenAIBaseURL and customOpenAIKey were none,
 If you installed 0.3.3 and it caused openwebui to fail to start, please find the solution at author_url.
 0.3.5: Optimized the default values and the cleared content of the request body
+0.3.6: added Task bypass, refactored pipe to be async, added event emmiters, added propper emitter for the final results
+0.3.7: refactored to use aiohttp, added better sources formating
+0.3.8: removed response header.
 """
 
-from typing import List, Union
+from typing import List, Union, Dict
 from pydantic import BaseModel, Field
-import requests
+from dataclasses import dataclass
+from open_webui.constants import TASKS
+from open_webui.utils.chat import generate_chat_completion
+import aiohttp
+
+
+@dataclass
+class User:
+    id: str
+    email: str
+    name: str
+    role: str
+
+
+name = "Perplexica/"
+
 
 
 class Pipe:
@@ -58,11 +76,12 @@ class Pipe:
         customOpenAIKey: str = Field(
             default="default"
         )  # API key for custom OpenAI instance
+        task_model: str = Field(default="gpt-4o-mini")  # model tag for task bypass
 
     def __init__(self):
         self.type = "manifold"  # Pipe type
         self.id = "perplexica_pipe"  # Pipe ID
-        self.name = "Perplexica/"  # Pipe name
+        self.name = name  # Pipe name
         self.valves = self.Valves()  # Initialize Valves instance
         self.history = []  # Initialize history list for storing conversation history
 
@@ -78,55 +97,87 @@ class Pipe:
             )
         return enabled_pipes
 
-    def pipe(self, body: dict, results=None) -> Union[str, dict]:
+    async def emit_status(self, level: str, message: str, done: bool):
+        await self.__current_event_emitter__(
+            {
+                "type": "status",
+                "data": {
+                    "status": "complete" if done else "in_progress",
+                    "level": level,
+                    "description": message,
+                    "done": done,
+                },
+            }
+        )
+
+    async def emit_message(self, message: str):
+        await self.__current_event_emitter__(
+            {"type": "message", "data": {"content": message}}
+        )
+
+    async def pipe(
+        self,
+        body: dict,
+        __user__: dict,
+        __event_emitter__=None,
+        __task__=None,
+        __model__=None,
+        __request__=None,
+        results=None,
+    ) -> Union[str, dict]:
         """Process the request and return the search results."""
-        user_input = self._extract_user_input(
-            body
-        )  # Extract user input from the request body
+        user_input = self._extract_user_input(body)
+        model = self.valves.task_model
+        self.__user__ = User(**__user__)
+        self.__request__ = __request__
+        self.__current_event_emitter__ = __event_emitter__
+        if __task__ and __task__ != TASKS.DEFAULT:
+            response = await generate_chat_completion(
+                self.__request__,
+                {"model": model, "messages": body.get("messages"), "stream": False},
+                user=self.__user__,
+            )
+            return f"{name}: {response['choices'][0]['message']['content']}"
         if not user_input:
             return "No search query provided"  # Return an error message if no user input is provided
 
-        model = body.get("model", "")  # Get the model name from the request body
-        print(f"Received model: {model}")  # Print the received model name
+        model = body.get("model", "")
+        print(f"Received model: {model}")
 
         if "perplexica" in model.lower() and self.valves.enable_perplexica:
+            await self.emit_status("info", "Searching...", False)
             print("Calling Perplexica search")
-            response = self._search_perplexica(
-                user_input, results
-            )  # Get search results
-            self._update_history(user_input, response)  # Update conversation history
+            response = await self._search_perplexica(user_input, results)
+            self._update_history(user_input, response)
+            await self.emit_message(response)
+            print(response)
+            await self.emit_status("info", "Search Completed", True)
             return response  # Return the search results
         else:
-            return f"Unsupported or disabled search engine for model: {model}"  # Return an error message if the search engine is not supported or disabled
+            return f"Unsupported or disabled search engine for model: {model}"
 
     def _update_history(self, user_input: str, response: str):
         """Update the conversation history."""
-        self.history.append(["human", user_input])  # Add user input to history
-        self.history.append(["assistant", response])  # Add search results to history
+        self.history.append(["human", user_input])
+        self.history.append(["assistant", response])
 
     def _extract_user_input(self, body: dict) -> str:
         """Extract user input from the request body."""
-        messages = body.get(
-            "messages", []
-        )  # Get the messages list from the request body
+        messages = body.get("messages", [])
         if messages:
-            last_message = messages[-1]  # Get the last message in the list
-            if isinstance(
-                last_message.get("content"), list
-            ):  # Check if the content of the last message is a list
-                for item in last_message["content"]:  # Iterate through the list
-                    if item["type"] == "text":  # Check if the item type is "text"
-                        return item["text"]  # Return the text content of the item
+            last_message = messages[-1]
+            if isinstance(last_message.get("content"), list):
+                for item in last_message["content"]:
+                    if item["type"] == "text":
+                        return item["text"]
             else:
-                return last_message.get(
-                    "content", ""
-                )  # Return the content of the last message if it is not a list
-        return ""  # Return an empty string if no user input is found
+                return last_message.get("content", "")
+        return ""
 
-    def _search_perplexica(self, query: str, results=None) -> str:
+    async def _search_perplexica(self, query: str, results=None) -> str:
         """Execute a search using the Perplexica Search API."""
         if not self.valves.enable_perplexica:
-            return "Perplexica search is disabled"  # Return an error message if Perplexica search is disabled
+            return "Perplexica search is disabled"
 
         print("Searching with Perplexica")
         try:
@@ -135,7 +186,6 @@ class Pipe:
                 "chatModel": {
                     "provider": self.valves.perplexica_chat_provider,
                     "model": self.valves.perplexica_chat_model,
-                    # Add custom OpenAI configuration if provided
                     "customOpenAIBaseURL": self.valves.customOpenAIBaseURL,
                     "customOpenAIKey": self.valves.customOpenAIKey,
                 },
@@ -143,49 +193,54 @@ class Pipe:
                     "provider": self.valves.perplexica_embedding_provider,
                     "model": self.valves.perplexica_embedding_model,
                 },
-                "optimizationMode": self.valves.perplexica_optimization_mode,  # Use the configured optimization mode
+                "optimizationMode": self.valves.perplexica_optimization_mode,
                 "focusMode": self.valves.perplexica_focus_mode,
-                "query": query,  # User search query
-                "history": self.history,  # Add history parameter
+                "query": query,
+                "history": self.history,
             }
 
-            # Remove None values from request_body
+            # Remove None values and "default" strings from request_body
             request_body = {k: v for k, v in request_body.items() if v is not None}
             request_body = {k: v for k, v in request_body.items() if v != "default"}
-            headers = {"Content-Type": "application/json"}  # Set request headers
+            headers = {"Content-Type": "application/json"}
 
-            response = requests.post(
-                self.valves.perplexica_api_url,
-                json=request_body,
-                headers=headers,  # Send POST request to Perplexica API
+            async with aiohttp.ClientSession() as session:
+                async with session.post(
+                    self.valves.perplexica_api_url, json=request_body, headers=headers
+                ) as response:
+                    response.raise_for_status()
+                    search_results = await response.json()
+
+            # Use the number of sources (n_sources) instead of a timer
+            n_sources = len(search_results.get("sources", []))
+
+            # Build the details block that now includes the sources inside it
+            header = f"Web search found {n_sources} sources"
+            details_block = (
+                f'<details type="web search" done="true" count="{n_sources}">\n'
+                f"<summary>{header}</summary>\n"
+                f"> {n_sources} sources found\n\n"
             )
-            response.raise_for_status()  # Raise an exception if the request failed
+            for i, source in enumerate(search_results.get("sources", []), 1):
+                title = source["metadata"].get("title", "No title")
+                snippet = source.get("pageContent", "No snippet available")
+                link = source["metadata"].get("url", "No URL available")
+                details_block += f"{i}. {title}\n URL: {link}\n\n"
+            details_block += "</details>\n\n"
 
-            search_results = response.json()  # Parse the response JSON data
+            # Retrieve and clean the message
+            message_content = search_results.get("message", "No message available")
+            # If message starts exactly with "Perplexica Search Results:", remove it
+            prefix = "Perplexica Search Results:"
+            if message_content.startswith(prefix):
+                message_content = message_content[len(prefix) :].lstrip()
 
-            # Formatted result
-            formatted_results = (
-                f"Perplexica Search Results:\n\n"  # Initialize formatted results string
-            )
-            formatted_results += (
-                search_results.get("message", "No message available")
-                + "\n\n"  # Add the message from the search results
-            )
-            for i, source in enumerate(
-                search_results.get("sources", []), 1
-            ):  # Iterate through the sources in the search results
-                title = source["metadata"].get(
-                    "title", "No title"
-                )  # Get the title of the source
-                snippet = source.get(
-                    "pageContent", "No snippet available"
-                )  # Get the snippet of the source
-                link = source["metadata"].get(
-                    "url", "No URL available"
-                )  # Get the URL of the source
-                formatted_results += f"{i}. {title}\n URL: {link}\n\n"  # Add the formatted source information to the results string
+            # Build the final formatted result string
+            formatted_results = details_block
+            formatted_results += message_content + "\n\n"
 
-            return formatted_results  # Return the formatted results string
+            return formatted_results
 
         except Exception as e:
-            return f"An error occurred while searching Perplexica: {str(e)}"  # Return an error message if an exception occurred during the search
+            await self.emit_status("info", f"Error: {str(e)}", True)
+            return "An error occurred during the Perplexica search."
